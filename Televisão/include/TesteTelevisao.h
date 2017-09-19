000de97de0304366df2d1c29615c747d8677269b
luizbob <luizhenrique@eng.ci.ufpb.br>
2017-09-19 11:34:50 -0300

    Questao dois

---
+++ Televisão/include/TesteTelevisao.h
@@ -0,0 +1,18 @@
+#ifndef TESTETELEVISAO_H
+#define TESTETELEVISAO_H
+#include "Televisao.h"
+
+
+class TesteTelevisao
+{
+    public:
+        Televisao tv1;
+        TesteTelevisao();
+        virtual ~TesteTelevisao();
+
+    protected:
+
+    private:
+};
+
+#endif // TESTETELEVISAO_H
