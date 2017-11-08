e23570bcf29eaab47e2c7208a4814d5e55e550bc
JoviNoob <30627485+JoviNoob@users.noreply.github.com>
2017-11-07 22:33:50 -0300

    Apagando Questao Folha de Pagamento

--- Roteiro-8/FolhaDePagamento/FolhaPagamento/src/Assalariado.cpp
+++
@@ -1,8 +0,0 @@
-#include "Assalariado.h"
-
-double Assalariado::calculaSalario(){ return salario; }
-Assalariado::Assalariado(string nome, int matricula, double salario){
-    this->setNome(nome);
-    this->setMatricula(matricula);
-    this->salario = salario;
-}
