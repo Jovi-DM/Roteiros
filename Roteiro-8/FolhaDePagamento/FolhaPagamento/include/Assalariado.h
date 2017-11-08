b9eb69a1078a207456430de6f8d74d660cc68972
JoviNoob <30627485+JoviNoob@users.noreply.github.com>
2017-11-07 22:20:01 -0300

    Questão 3 funcionando com menu!

---
+++ Roteiro-8/FolhaDePagamento/FolhaPagamento/include/Assalariado.h
@@ -0,0 +1,16 @@
+#ifndef ASSALARIADO_H
+#define ASSALARIADO_H
+
+#include "Funcionario.h"
+
+class Assalariado: public Funcionario
+{
+    public:
+        double calculaSalario();
+        Assalariado( string nome, int matricula, double salario);
+
+    private:
+        double salario;
+};
+
+#endif // ASSALARIADO_H
